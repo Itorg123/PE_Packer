c968a85f2ec521feee1e045d6f7c29e7d3d0e0fb
Itorg123 <itay@DESKTOP-83FH888.localdomain>
2025-06-25 23:05:03 +0300

    change in add_section.py, read .encrypt in the loader (un_packer.c)

--- un_packer.c
+++ un_packer.c
@@ -15,16 +15,77 @@
   _| |_   | |/ ____ \| |          | |  | |/ ____ \ _| |_| . \| |____
  |_____|  |_/_/    \_\_|          |_|  |_/_/    \_\_____|_|\_\______|
 */
+
+char i_key = 'J';
+char y_key = 'x';
+
+char help_to_decrypt_2(int i)
+{
+    switch (i)
+    {
+    case 0:
+        return i_key;   // I
+    case 1:
+        return 's';     // t
+    case 2:
+        return 'b';     // a
+    case 3:
+        return y_key;   // y
+    case 4:
+        return '`';     // _
+    case 5:
+        return 'g';     // h
+    }
+}
+
+void help_to_decrypt(int i, char key_arr[])
+{
+    // for any i - find the correct char
+
+    if (i % 2 == 0)
+    {
+        key_arr[i] = help_to_decrypt_2(i) - 1;
+    }
+    else
+    {
+        key_arr[i] = help_to_decrypt_2(i) + 1;
+    }
+}
+
+
+
+void decrypt(unsigned char *encrypted_data, int size)
+{
+    char key[7] = ""; // "Itay_h"
+
+    for (int i = 0; i < 6; i++)
+    {
+        help_to_decrypt(i, key);
+    }
+
+    // decrypt
+
+    for (int i = 0; i < size; i++) 
+    {
+        for (int j = 0; j < 6; j++) 
+        {
+            encrypted_data[i] ^= (unsigned char)key[j];
+        }
+    }
+
+}
+
+
+
 int main()
 {
     // Read the exe -> read the .encrypt section -> decrypt -> run 
 
-
     // open file
-
     FILE* exe;
     
-    if (fopen_s(&exe, "blabla.exe", "rb") == 0 || !exe);
+    fopen_s(&exe, "C:\\Users\\Itay H\\Desktop\\cyber\\packer_project_learn\\PACKER_00.exe", "rb");
+    if (!exe)
     {
         printf("Open file failed");
         return 0;
@@ -51,13 +112,14 @@ int main()
     PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)buffer;
 
     // nt_header
-    PIMAGE_NT_HEADERS nt_header = (PIMAGE_NT_HEADERS)buffer + dos_header->e_lfanew;
+    PIMAGE_NT_HEADERS nt_header = (PIMAGE_NT_HEADERS)(buffer + dos_header->e_lfanew);
 
     // file_header
-    PIMAGE_FILE_HEADER file_header = (PIMAGE_FILE_HEADER)(nt_header->FileHeader);
+    IMAGE_FILE_HEADER file_header = (IMAGE_FILE_HEADER)(nt_header->FileHeader);
 
     // number of sections
-    int section_number = file_header->NumberOfSections;
+    int section_number = file_header.NumberOfSections;
+
 
     int size_of_section_header = sizeof(IMAGE_SECTION_HEADER);
     
@@ -66,18 +128,26 @@ int main()
 
     for (int i = 0; i < section_number; i++) 
     {
-        if (section_header->Name == ".encrypt")
+        if (!memcmp(section_header->Name, ".encrypt", strlen(".encrypt")))
         {
             // size, offset of header
             int encrypt_offset = section_header->PointerToRawData;
             int encrypt_size = section_header->SizeOfRawData;
             
+            // start from the section offset
             fseek(exe, encrypt_offset, SEEK_SET);
 
-            // 
-        }
-    }
-    // section_header.  
+            // read to buffer
+            unsigned char* enc = (unsigned char*)malloc(encrypt_size);
+            fread(enc, 1, encrypt_size, exe);
 
+            // decrypt
+            decrypt(enc, encrypt_size);
+            
+            break;
+        }
+
+        section_header++;
+    }
 
 }
