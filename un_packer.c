f8459e5e6416dc4af84fa6e1f9b418a13c30566b
Itorg123 <itay@DESKTOP-83FH888.localdomain>
2025-06-26 23:38:16 +0300

    Loader - Load to image and start relocation

--- un_packer.c
+++ un_packer.c
@@ -1,9 +1,8 @@
-// PACKER.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <windows.h>
 #include <winnt.h>
 
@@ -82,6 +81,9 @@ int main()
     // Read the exe -> read the .encrypt section -> decrypt -> run 
 
     // open file
+
+    // open thread of find debuger - run all time
+
     FILE* exe;
     
     fopen_s(&exe, "C:\\Users\\Itay H\\Desktop\\cyber\\packer_project_learn\\PACKER_00.exe", "rb");
@@ -100,13 +102,7 @@ int main()
     // read to buffer
 
     unsigned char * buffer = (unsigned char *)malloc(this_size);
-    fread(buffer, 1, this_size, exe); // 1 byte * size
-
-    // dos_header -> nt_header -> file_header
-    //                   ||          
-    //                   \/
-    //                 optional_header -> 
-    
+    fread(buffer, 1, this_size, exe); // 1 byte * size    
 
     // dos_header
     PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)buffer;
@@ -120,11 +116,11 @@ int main()
     // number of sections
     int section_number = file_header.NumberOfSections;
 
-
-    int size_of_section_header = sizeof(IMAGE_SECTION_HEADER);
-    
+    // secion header
     PIMAGE_SECTION_HEADER section_header = IMAGE_FIRST_SECTION(nt_header);
 
+    //init the buffer
+    unsigned char* enc = NULL;
 
     for (int i = 0; i < section_number; i++) 
     {
@@ -138,7 +134,7 @@ int main()
             fseek(exe, encrypt_offset, SEEK_SET);
 
             // read to buffer
-            unsigned char* enc = (unsigned char*)malloc(encrypt_size);
+            enc = (unsigned char*)malloc(encrypt_size);
             fread(enc, 1, encrypt_size, exe);
 
             // decrypt
@@ -147,7 +143,100 @@ int main()
             break;
         }
 
+        // next section
         section_header++;
     }
+    
+    /*
+    .....................................................................................................................
+                                                     _                    _           
+                                                    | |    ___   __ _  __| | ___ _ __ 
+                                                    | |   / _ \ / _` |/ _` |/ _ \ '__|
+                                                    | |__| (_) | (_| | (_| |  __/ |   
+                                                    |_____\___/ \__,_|\__,_|\___|_|   
+    .....................................................................................................................
+    */
+
+    // dos_header
+    dos_header = (PIMAGE_DOS_HEADER)enc;
+
+    // nt_header
+    nt_header = (PIMAGE_NT_HEADERS)(enc + dos_header->e_lfanew);
+
+    // file_header
+    file_header = (IMAGE_FILE_HEADER)(nt_header->FileHeader);
+
+    // optional header
+
+    IMAGE_OPTIONAL_HEADER optional_header = (IMAGE_OPTIONAL_HEADER)(nt_header->OptionalHeader);
+
+    // image size
+    int size_of_image = optional_header.SizeOfImage;
+
+    // allocate image
+
+    LPVOID virtual_memory = VirtualAlloc(NULL, // windows choose the address
+                                         size_of_image, // size to allocate
+                                         MEM_COMMIT | MEM_RESERVE, // allocate physical memory
+                                         PAGE_EXECUTE_READWRITE); // read, write, execute
+    
+    // image base
+    uintptr_t image_base = optional_header.ImageBase;
+
+
+    // printf("image base: %llx, actual address: %llx, delta: %llx", image_base, virtual_memory, delta);
+
+    // copy headers to memory
+    memcpy(virtual_memory, enc, optional_header.SizeOfHeaders);
+
+
+    // number of sections
+    section_number = file_header.NumberOfSections;
+
+    // secion header
+    section_header = IMAGE_FIRST_SECTION(nt_header);
+
+
+    for (int i = 0; i < section_number; i++)
+    {
+        // size, offset of header
+        BYTE* source_address = (BYTE*)virtual_memory + section_header->VirtualAddress;
+        BYTE* destination_address = (BYTE*)enc + section_header->PointerToRawData;
+        size_t size_of_section = (size_t)section_header->SizeOfRawData;
+        memcpy(destination_address, source_address, size_of_section);
+
+        // next section
+        section_header++;
+    }
+
+    
+    // relocation
+
+    if (image_base != (uintptr_t)virtual_memory)
+    {
+        // delta between actual address and "wanted"
+        uintptr_t delta = (uintptr_t)virtual_memory - image_base;
+
+        // realoc directory
+        IMAGE_DATA_DIRECTORY realoc_dir = optional_header.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC];
+        uintptr_t realoc_dir_size = realoc_dir.Size;
+        uintptr_t realoc_dir_rva = realoc_dir.VirtualAddress;
+
+        // realoc .......
+        PIMAGE_BASE_RELOCATION realoc = (PIMAGE_BASE_RELOCATION)((BYTE*)virtual_memory + realoc_dir_rva);
+
+        int passed = 0;
+
+        while (passed < realoc_dir_size) 
+        {
+
+
+            passed += realoc->SizeOfBlock;
+        }
+
+
+        
+    }
+
 
 }
